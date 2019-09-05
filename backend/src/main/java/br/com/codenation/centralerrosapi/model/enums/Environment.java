de8f2b34c6a94d5a3fa570c59e8bd0f94f694848
Luis Fernando Carvalho <luiscvlh11@gmail.com>
2019-09-04 21:10:59 -0300

    Finalizar issue

--- backend/src/main/java/br/com/codenation/centralerrosapi/model/enums/Environment.java
+++
@@ -1,7 +0,0 @@
-package br.com.codenation.centralerrosapi.model.enums;
-
-public enum Environment {
-
-    DEVELOPMENT, TEST, PRODUCTION
-
-}
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import FormLogin from "@/views/Login/Form";
+import FormRegister from "@/views/Register/Form";
+
 Vue.use(Router);
 
 export default new Router({
@@ -8,9 +11,14 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home"
-      //component: Home
+      path: "/login",
+      name: "login",
+      component: FormLogin
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: FormRegister
     }
   ]
 });
